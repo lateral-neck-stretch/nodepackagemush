909f6a64ee0c6a97798fdcb7cf8e7cc33ebc1383
Anthony Zhang <zhang.anthony97@gmail.com>
2022-11-20 15:07:41 -0500

    added initial mushroom frame, linked up with anime.js

--- client/App.js
+++ client/App.js
@@ -1,15 +1,43 @@
-import React from 'react'
+import React from "react";
+import anime from "animejs/lib/anime.es.js";
 
-import Navbar from './components/Navbar'
-import Routes from './Routes'
+import Navbar from "./components/Navbar";
+import Routes from "./Routes";
 
-const App = () => {
+// let animation = anime({
+//   targets: ".bud_bounce",
+//   // Properties
+//   translateX: 100,
+//   borderRadius: 50,
+//   // Property Parameters
+//   duration: 2000,
+//   easing: "linear",
+//   // Animation Parameters
+//   direction: "alternate",
+// });
+
+function App() {
+  const animationRef = React.useRef(null);
+  React.useEffect(() => {
+    animationRef.current = anime({
+      targets: ".bud_bounce",
+      // Properties
+      // translateX: 100,
+      borderRadius: 50,
+      // Property Parameters
+      duration: 2000,
+      easing: "linear",
+      // Animation Parameters
+      direction: "alternate",
+      loop: true,
+      scale: 1.2,
+    });
+  });
   return (
-    <div>
-      <Navbar />
-      <Routes />
+    <div className="bud_bounce">
+      <img src="bud_bounce.svg" alt="bud bounce" />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
