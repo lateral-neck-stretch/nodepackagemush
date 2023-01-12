ff421e70979ffc434982e0a05fbd50c1bd65c780
Anthony Zhang <zhang.anthony97@gmail.com>
2023-01-12 16:02:38 -0500

    created mush db model

---
+++ server/db/models/Mush.js
@@ -0,0 +1,33 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const axios = require('axios');
+
+const SALT_ROUNDS = 5;
+
+const Mush = db.define('mush', {
+  name: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  minLat: {
+    type: Sequelize.FLOAT,
+    allowNull: false,
+  },
+  maxLat: {
+    type: Sequelize.FLOAT,
+    allowNull: false,
+  },
+  minLong: {
+    type: Sequelize.FLOAT,
+    allowNull: false,
+  },
+  maxLong: {
+    type: Sequelize.FLOAT,
+    allowNull: false,
+  },
+});
+
+module.exports = Mush;
